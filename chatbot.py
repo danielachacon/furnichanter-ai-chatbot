b07f062bc237167d6e14dfd666fafcfd734dae33
danielachacon <e.danielachacon@gmail.com>
2024-11-03 18:15:22 -0500

    Add chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,11 @@
+parameters = {
+  'type': None,
+  'color': None,
+  'material': None,
+  'size': None
+}
+
+text = input("How can I help you?")
+
+while 1:
+  text = input("Please enter parameters")
